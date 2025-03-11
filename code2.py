d13a71c9f79a0a4c9bc77cbc85a741b9bdb29e22
Jatin Sabari <jatinsabaritech@gmail.com>
2025-03-11 20:27:13 +0530

    Update code2.py

--- code2.py
+++ code2.py
@@ -1,14 +1,22 @@
 #By considering the terms in the fibonacci sequence whoe values do not exceed four million, find the sum of the even valued terms?
-set1 = set()
-i = 0
+set1 = [1, 2]
+set2 = []
+n = 0
+f = 1
+j = 0
+k = 0
+while j < 4000000:
+    j = set1[n] + set1[f]
+    set1.append(j)
+    n += 1
+    f += 1
+
+while k < len(set1):
+    if set1[k] % 2 == 0:
+        set2.append(set1[k])
+    k += 1
 total = 0
-while i< 1000:
-    if i%3 == 0:
-        set1.add(i)
-    elif i%5 == 0:
-        set1.add(i)
-    i+=1
-for n in set1:
-    total += n
-        
-print(total)
+for number in set2:
+    total += number
+
+print(f"{total}")
